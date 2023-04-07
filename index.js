165681002f43aadcd2cd0ce7962b95efd6a6d66f
Daria Hryshchenko <daria199210@gmail.com>
2023-04-07 23:52:30 +0300

    add code to add/get/remove contacts from contacts.json

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const contacts = require("./contacts");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+      break;
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      return console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const arr = hideBin(process.argv);
+console.log(arr);
+const { argv } = yargs(arr);
+invokeAction(argv);
